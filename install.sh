2c38e8ba983f4efb3a3fa0cc02809b941096061c
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-21 16:00:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ root_password=$1
 sudo mkdir ~/docker-registry
 cd ~/docker-registry
 sudo mkdir registry-data auth certs
-docker run --rm --entrypoint htpasswd registry:2 -Bbn root "$root_password" > ~/docker-registry/auth/registry.password
+sudo docker run --rm --entrypoint htpasswd registry:2 -Bbn root "$root_password" > ~/docker-registry/auth/registry.password
 sudo curl -o docker-compose.yaml https://raw.githubusercontent.com/WildePizza/docker-registry/HEAD/docker-compose.yaml
 sudo docker compose up -d
