1032e53adfc67c59e1201de683488dfd348e053b
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-21 18:03:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,16 @@ sudo mkdir ~/docker-registry
 cd ~/docker-registry
 sudo mkdir registry auth
 sudo docker run --rm --entrypoint htpasswd registry:2 -Bbn root "$root_password" > ~/docker-registry/auth/registry.password
-sudo docker run -d -p 718:5000 --network host --restart=always --name registry \
+sudo docker run -d -p 5000:5000 --network host --restart=always --name docker-registry \
 -e REGISTRY_AUTH="htpasswd" \
 -e REGISTRY_AUTH_HTPASSWD_REALM="Registry" \
 -e REGISTRY_AUTH_HTPASSWD_PATH="/auth/registry.password" \
 -e REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY="/registry" \
 -v ./registry:/registry \
 -v ./auth:/auth registry:2
+docker run --name docker-registry-ui \
+  -d --network host --restart=always \
+  -e ENV_DOCKER_REGISTRY_HOST=127.0.0.1 \
+  -e ENV_DOCKER_REGISTRY_PORT=5000 \
+  konradkleine/docker-registry-frontend:v2
 sudo docker compose up -d
