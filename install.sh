0c0afae413afb4d66ae79930b1dd17d81ba6ae5a
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-21 12:49:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+root_password=$1
+
 sudo mkdir ~/docker-registry
 cd ~/docker-registry
 sudo mkdir registry-data auth
 cd auth
 sudo apt install apache2-utils -y
-sudo htpasswd -b -c registry.password admin password
+sudo htpasswd -b -c registry.password root $root_password
 cd ..
 sudo curl -o docker-compose.yaml https://raw.githubusercontent.com/WildePizza/docker-registry/HEAD/docker-compose.yaml
 sudo docker compose up -d
