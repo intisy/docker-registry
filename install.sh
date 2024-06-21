369a29b287b224b74e35657cdc1dd163e6f9c441
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-21 21:53:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo docker run -d -p 5000:5000 --restart=always --name docker-registry --networ
   -v ./auth:/auth \
   -v $(pwd)/registry:/var/lib/registry \
   registry:2.7
-sudo docker run --rm -it --name docker-registry-ui --network registry 
+sudo docker run --rm -it --name docker-registry-ui --network registry \
   -e ENV_DOCKER_REGISTRY_HOST=docker-registry \
   -e ENV_REGISTRY_PORT=5000 -p 8080:80 \
   konradkleine/docker-registry-frontend:v2
