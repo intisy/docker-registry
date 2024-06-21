35f5e17b7d058e974115ab3a7f44e597334ad37b
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-21 20:03:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ cd ~/docker-registry
 sudo mkdir registry auth
 cd auth
 sudo apt install apache2-utils -y
-sudo htpasswd -b -c registry.password admin password
+sudo htpasswd -b -c auth.htpasswd root root
 cd ..
 sudo docker run -d -p 5000:5000 --restart=always --name docker-registry \
   -e REGISTRY_AUTH=htpasswd \
-  -e REGISTRY_AUTH_HTPASSWD_PATH=/registry/auth.htpasswd \
-  -v ./auth/registry.htpasswd:/registry/auth.htpasswd \
+  -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/auth.htpasswd \
+  -v ./auth:/auth \
   -v ./registry:/registry \
   registry:2.7
