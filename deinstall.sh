8b549e3357cc725d9547e5085aaacb919e40ea47
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-21 18:57:43 +0200

    Update deinstall.sh

--- deinstall.sh
+++ deinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=registry --format="{{.ID}}"))
+sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=konradkleine/docker-registry-frontend:v2 --format="{{.ID}}"))
 sudo rm -r ~/docker-registry
