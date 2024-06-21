205cffd9b5e693a4a27b4a45c58bde09d6493f2b
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-21 19:11:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ root_password=$1
 sudo mkdir ~/docker-registry
 cd ~/docker-registry
 sudo mkdir registry auth
+cd auth
+htpasswd -cB registry.htpasswd root root
+cd ..
 sudo docker run -d -p 5000:5000 --restart=always --name docker-registry \
-  -e REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY="/registry" \
+  -e REGISTRY_AUTH=htpasswd \
+  -e REGISTRY_AUTH_HTPASSWD_PATH=/registry/auth.htpasswd \
+  -v ./auth/registry.htpasswd:/registry/auth.htpasswd \
   -v ./registry:/registry \
   registry:2.7
