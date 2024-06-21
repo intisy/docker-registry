9ca7d2adae3e60ce158abba0603bdb109d9904d2
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-21 17:32:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,11 @@ sudo mkdir ~/docker-registry
 cd ~/docker-registry
 sudo mkdir registry-data auth certs
 sudo docker run --rm --entrypoint htpasswd registry:2 -Bbn root "$root_password" > ~/docker-registry/auth/registry.password
-sudo curl -o docker-compose.yaml https://raw.githubusercontent.com/WildePizza/docker-registry/HEAD/docker-compose.yaml
+sudo docker run -d -p 718:5000 --network host --restart=always --name registry \
+-e REGISTRY_AUTH="htpasswd" \
+-e REGISTRY_AUTH_HTPASSWD_REALM="Registry" \
+-e REGISTRY_AUTH_HTPASSWD_PATH="/auth/registry.password" \
+-e REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY="/registry" \
+-v ./registry:/registry
+-v ./auth:/auth registry:2
 sudo docker compose up -d
