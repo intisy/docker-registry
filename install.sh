2f7bf4e91b8e6eacd6237ff35b48e8b3af8eb959
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-21 17:47:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ sudo docker run -d -p 718:5000 --network host --restart=always --name registry \
 -e REGISTRY_AUTH_HTPASSWD_REALM="Registry" \
 -e REGISTRY_AUTH_HTPASSWD_PATH="/auth/registry.password" \
 -e REGISTRY_STORAGE_FILESYSTEM_ROOTDIRECTORY="/registry" \
--v ./registry:/registry
+-v ./registry:/registry \
 -v ./auth:/auth registry:2
 sudo docker compose up -d
