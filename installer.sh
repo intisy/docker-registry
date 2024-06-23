c26748cf55763499e8033211d0d9e05b192596ed
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-23 19:15:56 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,9 +4,10 @@ args=$@
 
 execute() {
   sha=$(curl -sSL https://api.github.com/repos/WildePizza/docker-registry/commits?per_page=2 | jq -r '.[1].sha')
-  url="https://raw.githubusercontent.com/WildePizza/docker-registry/HEAD/.commits/$sha/install.sh"
+  url="https://raw.githubusercontent.com/WildePizza/docker-registry/HEAD/.commits/$sha/install.shh"
   echo "Executing: $url"
   curl -fsSL $url | bash -s $args
+  echo $?
   if [[ $? -ne 0 ]]; then
     execute
   fi
