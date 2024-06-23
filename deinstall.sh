d5cf7abd57bcae89b033e8a5f90b038dcc3b0371
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-23 21:57:32 +0200

    Update deinstall.sh

--- deinstall.sh
+++ deinstall.sh
@@ -14,5 +14,5 @@ if [ "$using_kubernetes" = true ]; then
 else
   sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=registry:2.7 --format="{{.ID}}"))
   sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=konradkleine/docker-registry-frontend:v2 --format="{{.ID}}"))
-  sudo rm -r ~/docker-registry
 fi
+sudo rm -r ~/docker-registry
