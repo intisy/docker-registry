566b18300ce55fcb36fdcc3d07d51e69a0580a1d
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-06-23 18:56:44 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 args=$@
-curl -fsSL https://raw.githubusercontent.com/WildePizza/mysql-kubernetes/HEAD/.commits/install.sh | bash -s $args
+sha=$(curl -sSL https://api.github.com/repos/WildePizza/docker-registry/commits?per_page=2 | jq -r '.[1].sha')
+curl -fsSL https://raw.githubusercontent.com/WildePizza/mysql-kubernetes/HEAD/.commits/$sha/install.sh | bash -s $args
