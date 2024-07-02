b2e3d55129f6059e03da2b5f748d789a358c6ae7
Pizza <105643708+WildePizza@users.noreply.github.com>
2024-07-02 22:24:35 +0200

    Update deinstall.sh

--- scripts/deinstall.sh
+++ scripts/deinstall.sh
@@ -3,14 +3,14 @@
 using_kubernetes=true
 
 if [ "$using_kubernetes" = true ]; then
-  kubectl delete service docker-registry-ui
-  kubectl delete service docker-registry
-  kubectl delete deployment docker-registry
-  kubectl delete deployment docker-registry-ui
-  kubectl delete pvc docker-registry-data-pv-claim
-  kubectl delete pvc docker-registry-auth-pv-claim
-  kubectl delete pv docker-registry-data-pv
-  kubectl delete pv docker-registry-auth-pv
+  kubectl delete service docker-registry-ui --grace-period=0 --force
+  kubectl delete service docker-registry --grace-period=0 --force
+  kubectl delete deployment docker-registry --grace-period=0 --force
+  kubectl delete deployment docker-registry-ui --grace-period=0 --force
+  kubectl delete pvc docker-registry-data-pv-claim --grace-period=0 --force
+  kubectl delete pvc docker-registry-auth-pv-claim --grace-period=0 --force
+  kubectl delete pv docker-registry-data-pv --grace-period=0 --force
+  kubectl delete pv docker-registry-auth-pv --grace-period=0 --force
 else
   sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=registry:2.7 --format="{{.ID}}"))
   sudo docker rm $(sudo docker stop $(sudo docker ps -a -q --filter ancestor=konradkleine/docker-registry-frontend:v2 --format="{{.ID}}"))
