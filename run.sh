49511dfa1b4ba4023fa5efbd6607a767b6a33704
Pizza <wilderpizza99@gmail.com>
2024-07-04 18:13:30 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,16 +4,16 @@ action=$1
 arg=$2
 
 execute() {
-  substring="The requested URL returned error"
+  substring="#!/bin/bash"
   sha=$(curl -sSL https://api.github.com/repos/WildePizza/docker-registry/commits?per_page=2 | jq -r '.[1].sha')
   url="https://raw.githubusercontent.com/WildePizza/docker-registry/HEAD/.commits/$sha/scripts/$action.sh"
   echo "Executing: $url"
   output=$(curl -fsSL $url 2>&1)
   if [[ $output =~ $substring ]]; then
+    curl -fsSL $url | bash -s $sha $arg
+  else
     sleep 1
     execute
-  else
-    curl -fsSL $url | bash -s $sha $arg
   fi
 }
 execute
