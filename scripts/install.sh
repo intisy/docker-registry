8f5b524705e7febd073b5259513e5bd0a4b89c08
Pizza <wilderpizza99@gmail.com>
2024-07-04 18:49:05 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,6 +14,18 @@ generate_secure_password() {
   length=20
   password=$(openssl rand -base64 $length | tr -dc 'A-Za-z0-9')
 }
+wait_until_ready() {
+  url=$1
+  substring1="The requested URL returned error"
+  substring2="Could not resolve host: raw.githubusercontent.com"
+  echo "Executing: $url"
+  output=$(curl -fsSL $url 2>&1)
+  if [[ $output =~ $substring1 || $output =~ $substring2 ]]; then
+    sleep 1
+    wait_until_ready
+  fi
+}
+wait_until_ready https://raw.githubusercontent.com/WildePizza/docker-registry/HEAD/.commits/$sha/scripts/deinstall.sh
 curl -fsSL https://raw.githubusercontent.com/WildePizza/docker-registry/HEAD/.commits/$sha/scripts/deinstall.sh | bash -s
 if [ ! -n "$root_password" ]; then
   if [ "$gererate_password" = true ]; then
